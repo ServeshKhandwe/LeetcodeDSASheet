49e4d4fd9f224a826c5c7bf4ece2d8e8fc2d35c7
Servesh Khandwe <servesh1999@gmail.com>
2023-03-18 19:06:14 +0100

    217ContainsDuplicate (Hashset Method)

--- 217ContainsDuplicate.java
+++ 217ContainsDuplicate.java
@@ -1,12 +1,32 @@
+import java.util.HashSet;
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         for(int i=0;i<nums.length;i++){
+//             for(int j=i+1;j<nums.length;j++){
+//                 if(nums[i]==nums[j]){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// }
+// Upward Apporach is brute force On2
+
+// ideas we can sort as well Onlogn
+
+// We can use Hashset time On Space On
+
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]==nums[j]){
-                    return true;
-                }
-            }
+        HashSet<Integer> hashSet=new HashSet<>();
+
+        for(int n:nums){
+            if(hashSet.contains(n)) return true;
+            hashSet.add(n);
         }
+        
         return false;
     }
 }
