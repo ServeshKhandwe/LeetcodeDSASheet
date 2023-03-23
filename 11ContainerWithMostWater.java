c4eb93582f8992405bf83da5ece2279a4fc8ad08
Servesh Khandwe <servesh1999@gmail.com>
2023-03-23 17:33:37 +0100

    11ContainerWithMostWater(Kevin's Solution)

--- 11ContainerWithMostWater.java
+++ 11ContainerWithMostWater.java
@@ -4,7 +4,7 @@ class Solution {
         for(int i=0;i<height.length;i++){
             for(int j=i+1;j<height.length;j++){
                 int min = Math.min(height[i], height[j]);
-                max=Math.min(max, min*(j-i));
+                max=Math.max(max, min*(j-i));
             }
         }
         return max;
