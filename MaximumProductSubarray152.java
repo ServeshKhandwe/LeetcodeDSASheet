8c2afabea5e14ceb2752523a8d4d7b62fcbba578
Servesh Khandwe <servesh1999@gmail.com>
2023-03-26 18:55:00 +0200

    Max product Subarray 152 done

--- MaximumProductSubarray152.java
+++ MaximumProductSubarray152.java
@@ -2,14 +2,21 @@ import java.util.Arrays;
 
 class Solution {
     public int maxProduct(int[] nums) {
-        int product=0;
-        int currentProduct=1;
-
-        for(int i:nums){
-            if(currentProduct<0) currentProduct=1;
-            currentProduct*=i;
-            product=Math.max(product, currentProduct);
+        
+        int result = 0;
+        int currentMax =1;
+        int currentMin = 1;
+        for(int n :nums){
+            if(n==0){
+                currentMax=1;
+                currentMin=1;
+                continue;
+            }
+            int temp = currentMax*n;
+            currentMax=Math.max(Math.max(n*currentMax, n*currentMin),n);
+            currentMin=Math.min(Math.min(temp, n*currentMin),n);
+            result=Math.max(result,currentMax);
         }
-        return product;       
+        return result;
     }
 }
